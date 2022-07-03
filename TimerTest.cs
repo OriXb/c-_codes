9a2b54a5b1140ca0dab29a732541380a775d8b34
OriXb <107433373+OriXb@users.noreply.github.com>
2022-07-04 00:22:11 +0300

    Add files via upload

---
+++ TimerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerTest
+{
+
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer Timer;
+        int hours;
+        int minutes;
+        int seconds;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Timer.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer = new System.Timers.Timer();
+            Timer.Interval = 1000;
+            Timer.Elapsed += OnTimeEvent;
+
+        }
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            Timer.Stop();
+            Application.DoEvents();
+
+        }
+
+        private void OnTimeEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                seconds+= 1;
+                if(seconds==60)
+                {
+                    seconds = 0;
+                    minutes += 1;
+                }
+                if(minutes==60)
+                {
+                    minutes = 0;
+                    hours += 1;
+                }
+                text.Text = string.Format("{0}:{1}:{2}", hours.ToString().PadLeft(2, '0'), minutes.ToString().PadLeft(2, '0'), seconds.ToString().PadLeft(2,'0'));
+
+            }));
+        }
+    }
+}
---
+++ WeatherInfoTest.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WeatherBox = new System.Windows.Forms.TextBox();
+            this.EnterBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.EnterBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // WeatherBox
+            // 
+            this.WeatherBox.Location = new System.Drawing.Point(35, 21);
+            this.WeatherBox.Multiline = true;
+            this.WeatherBox.Name = "WeatherBox";
+            this.WeatherBox.Size = new System.Drawing.Size(727, 327);
+            this.WeatherBox.TabIndex = 0;
+            // 
+            // EnterBox
+            // 
+            this.EnterBox.Location = new System.Drawing.Point(167, 371);
+            this.EnterBox.Name = "EnterBox";
+            this.EnterBox.Size = new System.Drawing.Size(194, 20);
+            this.EnterBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EnterBox2
+            // 
+            this.EnterBox2.Location = new System.Drawing.Point(380, 371);
+            this.EnterBox2.Name = "EnterBox2";
+            this.EnterBox2.Size = new System.Drawing.Size(194, 20);
+            this.EnterBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EnterBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EnterBox);
+            this.Controls.Add(this.WeatherBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox WeatherBox;
+        private System.Windows.Forms.TextBox EnterBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox EnterBox2;
+    }
+}
+
